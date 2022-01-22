41782a22c4273bb693fc30d7708f9970fa73f226
Masha Gorbunova <masha.gorbunova@gmail.com>
2022-01-22 22:50:57 +0300

    add setting

--- src/Counter.stories.tsx
+++ src/Counter.stories.tsx
@@ -2,31 +2,46 @@ import React, {useState} from 'react';
 import App from "./App";
 import {UniversalButton} from "./UniversalButton";
 import s from "./Counter.module.css"
-
 export default {
     title: 'Counter story',
     component: App
 }
 
 export const Counter = () => {
-    let [state, setState] = useState(0)
+    let [minValue, setMinValue] = useState(0)
+    let [maxValue, setMaxValue] = useState(0)
+    let [state, setState] = useState(minValue)
     let [error, setError] = useState<string | null>(null)
     let [disableValue, setDisableValue] = useState(false)
 
-    const onClickIncHandler = () => {
 
-        if (state+1 < 5) {
-            setState(state+1)
+    const onChangeMinHandler = (e: any) => {
+        setMinValue(e.target.value)
+    }
+    const onChangeMaxHandler = (e: any) => {
+        setMaxValue(e.target.value)
+    }
+
+    const onClickIncHandler = () => {
+        if (state + 1 < Number(maxValue)) {
+            setState(Number(state) + 1)
         } else {
             setDisableValue(true)
-            setState(5)
+            setState(Number(maxValue))
+            console.log(Number(maxValue))
             setError('max value is reached')
         }
     }
 
-
     const onClickResetHandler = () => {
         setState(0)
+        setDisableValue(false)
+        setError('')
+    }
+
+    const onClickSetHandler = () => {
+        setState(Number(minValue))
+        setMaxValue(Number(maxValue))
     }
 
     return (
@@ -43,6 +58,18 @@ export const Counter = () => {
                 title='Reset'
                 callback={onClickResetHandler}
                 disabled={false}/>
+
+            <div>
+                <span>Max value</span>
+                <input type="number" min="0" max="100" value={maxValue} onChange={onChangeMaxHandler}/>
+            </div>
+
+            <div>
+                <span>Min value</span>
+                <input type="number" min="0" max="100" value={minValue} onChange={onChangeMinHandler}/>
+            </div>
+
+            <UniversalButton title="Set" callback={onClickSetHandler} disabled={false}/>
         </div>
     )
 }
